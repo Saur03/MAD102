0d9cbe71fd8e717117553aa3f7975148a0a1d18b
Saur03 <chawlasaurabh97@gmail.com>
2023-09-20 00:21:08 -0400

    Python [variables & types]

---
+++ Assignment1.py
@@ -0,0 +1,109 @@
+'''
+Question 1
+'''
+# Program Name: Create the appropriate variable
+# Purpose: To store the number of people that will fit in a car
+# Author: Saurabh Chawla
+# Date completed: 18th September, 2023
+
+# Program Introduction
+print('Store the number of people')
+
+# Declare variable
+numPeople = 5
+
+'''
+Question 2
+'''
+# Program Name: Create the appropriate variable
+# Purpose: To store a cell phone number
+# Author: Saurabh Chawla
+# Date completed: 18th September, 2023
+
+# Program Introduction
+print('Store the phone number')
+
+# Declare variable
+phoneNumber = 5555552323
+
+'''
+Question 3
+'''
+# Program Name: Create the appropriate variable
+# Purpose: To store the total number of fans
+# Author: Saurabh Chawla
+# Date completed: 18th September, 2023
+
+# Program Introduction
+print('Store the total number of fans in the arena')
+
+# Declare variable
+fanNumber = 19515
+
+'''
+Question 4
+'''
+# Program Name: Create the appropriate variable
+# Purpose: To store the US postal code
+# Author: Saurabh Chawla
+# Date completed: 18th September, 2023
+
+# Program Introduction
+print('Store the US postal code')
+
+# Declare variable
+postalCode = 90212
+
+'''
+Question 5
+'''
+# Program Name: Quote generating program
+# Purpose: Calculating total mural cost
+# Author: Saurabh Chawla
+# Date completed: 18th September, 2023
+
+# Declare variable
+charge = 1200.99
+
+# Program Introduction
+print('Welcome to the Quote generating program')
+
+# Get the variable values
+height = int(input('Please enter the height of the wall (in feet): '))
+width = int(input('Please enter the width of the wall (in feet): '))
+name = input('Please enter your name: ')
+
+# Calculate total cost
+total_cost = height*width*charge
+
+# Output results
+print(f'Name of the client is {name}')
+print(f'Height of the wall is {height}ft')
+print(f'Width of the wall is {width}ft')
+print(f'Total cost of mural is ${total_cost:.2f}')
+
+# Thank the user
+print('Thank you for using our program.')
+
+'''
+Question 6
+'''
+# Program Name: Lawn maintenance program
+# Purpose: Track money made each day
+# Author: Saurabh Chawla
+# Date completed: 18th September, 2023
+
+# Declare variable
+flat_rate = 65.35
+
+# Program Introduction
+print('Welcome to the Lawn maintenance program')
+
+# Get the variable values
+number = int(input('Please enter the number of lawns to mow each day: '))
+
+# Calculate total cost
+total_money_made = number*flat_rate
+
+# Output results
+print(f'Total money made is ${total_money_made:.2f}')
